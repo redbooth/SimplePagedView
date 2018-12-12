ccd470aa28fac08d0077a35fbb7c2fcb17cb8e4e
Alex Reilly <fabiobean2@gmail.com>
2018-12-12 10:59:00 -0800

    Initial Commit

---
+++ SimplePagedViewFramework/SimplePagedViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimplePagedViewFramework.h
+//  SimplePagedViewFramework
+//
+//  Created by New User on 12/12/18.
+//  Copyright © 2018 twof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimplePagedViewFramework.
+FOUNDATION_EXPORT double SimplePagedViewFrameworkVersionNumber;
+
+//! Project version string for SimplePagedViewFramework.
+FOUNDATION_EXPORT const unsigned char SimplePagedViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplePagedViewFramework/PublicHeader.h>
+
+
