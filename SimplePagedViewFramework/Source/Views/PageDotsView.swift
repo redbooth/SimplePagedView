5dfaa0f22133744264c8c99bda7c9f741bb680c0
Alex Reilly <fabiobean2@gmail.com>
2018-12-18 21:48:13 -0800

    extract dots view

--- SimplePagedViewFramework/Source/Views/PageDotsView.swift
+++ SimplePagedViewFramework/Source/Views/PageDotsView.swift
@@ -1,30 +1,5 @@
 import UIKit
 
-class DotView: UIControl {
-    let mainColor: UIColor
-
-    init(
-        frame: CGRect,
-        mainColor: UIColor = .black
-    ) {
-        self.mainColor = mainColor
-        super.init(frame: frame)
-        self.translatesAutoresizingMaskIntoConstraints = false
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func draw(_ rect: CGRect) {
-        let dotPath = UIBezierPath(ovalIn: rect)
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = dotPath.cgPath
-        shapeLayer.fillColor = mainColor.cgColor
-        layer.addSublayer(shapeLayer)
-    }
-}
-
 public class PageDotsView: UIView {
     private var dots: [DotView] = []
     private var dotCount: Int
